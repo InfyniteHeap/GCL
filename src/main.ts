82e042e608627ee1a7e8c8005ebb029cced97c90
InfyniteHeap <thebluealgae1123@outlook.com>
2023-11-29 00:31:26 +0800

    Update details.

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,11 @@ document.addEventListener('contextmenu', (event) => {
 
 // 禁用F1至F12功能键
 document.addEventListener('keydown', (event) => {
-    // 获取按下的键的键码
-    const keyCode = event.keyCode;
+    // 获取按下的键
+    const key = event.key;
 
     // 如果按下的键是F1至F12之间的键，则阻止默认行为
-    if (keyCode >= 112 && keyCode <= 123) {
+    if (/^F[1-12]$/.test(key)) {
         event.preventDefault();
     }
 });
