a1156eb05abba2b4028f6002ae495c275a0ed70e
InfyniteHeap <thebluealgae1123@outlook.com>
2023-11-28 23:25:07 +0800

    Update template.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,5 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import { createApp } from "vue";
+import "./styles.css";
+import App from "./App.vue";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  plugins: [vue()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
