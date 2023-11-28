650886688f08bbd911a5f94a74d8d4c2a24601e1
InfyniteHeap <thebluealgae1123@outlook.com>
2023-11-28 22:59:39 +0800

    Update template.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from "vue";
-import "./styles.css";
-import App from "./App.vue";
+import { invoke } from "@tauri-apps/api/tauri";
 
-createApp(App).mount("#app");
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
